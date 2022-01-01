8b0c1d80a151361f70ed1845628eb0e7e18b883e
gupta-divyam <90678273+gupta-divyam@users.noreply.github.com>
2022-01-01 15:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+namee="";
+function dod(){
+    namee=document.getElementById("")
+    localStorage.getItem("ppp1")
+    window.location.href = "letschat.html";
+}
+
+window.addEventListener("keydown", key_down)
+function key_down(e){
+    keyPressed=e.keyCode;
+   if (keyPressed== "13"){
+    namee=document.getElementById("")
+    localStorage.getItem("ppp1")
+    window.location.href = "letschat.html";
+   }
+}
